2a57558fbf8ae72840fe19b263a28a8f5a8e6dd4
oldDong <2232890213@qq.com>
2020-08-23 21:53:25 +0800

    watch监听

--- src/main.js
+++ src/main.js
@@ -1,67 +1,63 @@
 
 const Vue=window.Vue;
 Vue.config.productionTip = false;
-//传入名字和性别自动创建
-let id=0
-const createUser = (name,gender)=>{
-  id+=1
-  return {id:id,name:name,gender:gender}
-}
 
 new Vue({
   el:"#app",
-  data(){
-    return {
-      users:[
-        createUser('董阁','男'),
-        createUser('老黄','女'),
-        createUser('老李','男')
-      ],
-      gender:""
-    };
+  data:{
+    n:0,
+    history:[],
+    inUndoMode:false
   },
-  //计算属性
-  computed:{
-    displayUsers(){
-      const {users,gender}=this;
-      if(gender === ""){
-        return users;
-      }else if(gender ==='male'){
-        return users.filter(u=>u.gender ==='男');
-      }else if(gender === 'female'){
-        return users.filter(u=>u.gender ==='女');
-      }else{
-        return 0
+  //监听
+  watch:{
+    n(newValue,oldValue){
+      if(!this.inUndoMode){
+        this.history.push({from:oldValue,to:newValue});
       }
     }
   },
-  methods:{
-    showMale(){
-      //filter是筛选，不会修改原数组会创建一个新的
-      this.gender = 'male'
-    },
-    showFemale(){
-      this.gender = 'female'
 
-    },
-    showAll(){
-      this.gender = ''
-    }
-  },
 
   template:`
       <div>
-        <div>
-          {{displayName}}
-        </div>
+        {{n}}
         <hr>
-        <div>
-          <div><button @click="showAll">全部</button><button @click="showMale">男</button><button @click="showFemale">女</button></div>
-          <ul>
-            <li v-for="(u,index) in displayUsers" :key="index">{{u.name}}--{{u.gender}}</li>
-          </ul>
-        </div>
+        <button @click="add1">+1</button>
+        <button @click="add2">+2</button>
+        <button @click="minus1">-1</button>
+        <button @click="minus2">-2</button>
+        <hr>
+        <button @click="undo">撤销</button>
+        <hr>
+        {{history}}
       </div>
-      
-  `
+  `,
+  methods:{
+    add1(){
+      this.n+=1;
+    },
+    add2(){
+      this.n+=2;
+    },
+    minus1(){
+      this.n-=1;
+    },
+    minus2(){
+      this.n-=2;
+    },
+    undo(){
+      const last = this.history.pop();
+      console.log(last);
+      const old = last.from;
+      this.inUndoMode = true;
+      this.n = old;//watch是一异步的
+    //  异步执行，
+      this.$nextTick(()=>{
+        this.inUndoMode = false;
+      },0)
+
+    }
+  }
+
 })
